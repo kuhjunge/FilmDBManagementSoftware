98302008cfc883fac8b13863e2388cea79458f89
kuhjunge <quhberta@gmail.com>
2013-01-18 21:48:50 +0100

    - CSV Exportfunktion neu geschrieben

--- Folder_Sort/src/sort/choose.java
+++ Folder_Sort/src/sort/choose.java
@@ -2,6 +2,7 @@ package sort;
 import java.awt.FlowLayout;
 import java.awt.event.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -33,6 +34,68 @@ public class choose
 			}
 		}
 		return size;
+	}
+	
+	// Schreibt eine CSV mit einer Filmliste in den ausgewählten Ordner
+	private static void writeCSV(File f)
+	{
+		try
+			{
+				final File f1 = new File(f.getPath());	//In File speichern
+			    writer = new FileWriter(f.getPath() + "\\Filmliste.csv");
+		
+			    writer.append("Pfad: ;" + f.getPath() + "\n");
+				writer.append("Gesamtgröße ;" +(filesize(getDirSize(f1))) +" \n");
+				writer.append("-----------------\n");
+				
+				// Auslesen der Unterordner
+				
+				File[] files = f1.listFiles();
+				if (files != null) 
+				{ // Erforderliche Berechtigungen etc. sind vorhanden
+					for (int i = 0; i < files.length; i++) 
+					{
+						if (files[i].isDirectory()) 
+						{
+							String foldername = files[i].getName();
+							String[] splitFolder = foldername.split("_",4);
+							// Schreiben der einzelnen Filme in die CSV
+							//writer.append(foldername);
+
+							writer.append(splitFolder[splitFolder.length - 1]+ ";"); // Letzten Teil des Strings ausgeben
+							
+							// Vorne fehlende Daten leer setzen
+							for (int ii = 0; ii < (4 - splitFolder.length);ii++)
+							{
+								writer.append(";");
+							}
+							// Ausgabe des Split STrings
+							for (int ii = 0; ii < (splitFolder.length - 1); ii++) // Dann die restlichen Informationen im String ausgeben
+							{
+								writer.append(splitFolder[ii]+ ";");
+							}
+							// Ausgabe der größe
+							File m = new File (files[i].getAbsolutePath());
+							writer.append(";");
+							// Größenberechnung 
+							writer.append(filesize(getDirSize(m))); // Funktion Filesize gibt einen String mit Einhait zurück
+							writer.append("");
+						}
+					}
+				}
+			    writer.flush();
+			    writer.close();
+    			JOptionPane.showMessageDialog(null, "Speichern erfolgreich! \nDie CSV Datei liegt im Zielordner.");
+			}
+			catch (FileNotFoundException ex)
+			{
+				JOptionPane.showMessageDialog(null, "Der Zugriff auf die Datei ist gescheitert!");
+			}
+			catch(IOException e1)
+			{
+			     e1.printStackTrace();
+    			JOptionPane.showMessageDialog(null, "Es ist ein Fehler aufgetreten!");
+			} 	
 	}
 	
 	// Gibt die größe mit Einheit zurück
@@ -121,28 +184,13 @@ public class choose
       	  );
       	  
       	  btnCSV = new JButton("Save in CSV");
-      	  btnCSV.addActionListener
+      	  btnCSV.addActionListener // Listener für Button
       	  (
       			  new ActionListener()
       			  {
       				  public void actionPerformed(ActionEvent e)
       				  {
-      					try
-      					{
-      					    writer = new FileWriter(f.getPath() + "Filmliste.csv");
-      				
-      					    writer.append("Pfad: \n" + f.getPath() + "\n");
-      						writer.append("Gesamtgröße \n" +(getDirSize(f1)/1024/1024/1024)+ "GB \n");
-      						writer.append("------------------------------------------------------------------------------ \n");
-      				 
-      					    writer.flush();
-      					    writer.close();
-      					}
-      					catch(IOException e1)
-      					{
-      					     e1.printStackTrace();
-      					} 
-      			      	     
+      					 writeCSV(f); // Schreibt die CSV Datei
       				  }
       			  }
       	  );
